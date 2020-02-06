f8c47f1020abf8451b872b5d7436628b5a8d8bf6
cejutue <373580574@qq.com>
2020-02-06 16:25:57 +0800

    测试通过

--- emuntest2.cpp
+++ emuntest2.cpp
@@ -1,11 +1,43 @@
 #pragma once
 #include "stdafx.h"
 #include "enumtest.cpp"
-#include "Selector.h"
-int main()
-{
-	//CSelector* sel1 = new CSelector();
-	//CSelector * sel2 = new CSelector();
-	//CSelector* sel = new CBinarySelector(CBinarySelector::EUnion, sel1, sel2);
-	return  0;
+#include "gumbo-parser/Selector.h"
+#include "gumbo-parser/Document.h"
+#include "gumbo-parser/Selection.h"
+#include "gumbo-parser/Node.h"
+
+void test_parser() {
+	std::string page("<h1><a>wrong link</a><a class=\"special\"\\>some link</a></h1>");
+	CDocument doc;
+	doc.parse(page.c_str());
+
+	CSelection c = doc.find("h1 a.special");
+	CNode node = c.nodeAt(0);
+	printf("Node: %s\n", node.text().c_str());
+	std::string content = page.substr(node.startPos(), node.endPos() - node.startPos());
+	printf("Node: %s\n", content.c_str());
+}
+
+void test_html() {
+	std::string page = "<html><div><span>1\n</span>2\n</div></html>";
+	CDocument doc;
+	doc.parse(page.c_str());
+	CNode pNode = doc.find("div").nodeAt(0);
+	std::string content = page.substr(pNode.startPos(), pNode.endPos() - pNode.startPos());
+	printf("Node: #%s#\n", content.c_str());
+}
+
+void test_escape() {
+	std::string page = "<html><div><span id=\"that's\">1\n</span>2\n</div></html>";
+	CDocument doc;
+	doc.parse(page.c_str());
+	CNode pNode = doc.find("span[id=\"that's\"]").nodeAt(0);
+	std::string content = page.substr(pNode.startPos(), pNode.endPos() - pNode.startPos());
+	printf("Node: #%s#\n", content.c_str());
+}
+
+int main() {
+	test_parser();
+	test_html();
+	test_escape();
 }
